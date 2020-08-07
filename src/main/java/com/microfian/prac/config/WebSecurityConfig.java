d03eab3d7c71ad8fbbe6bb39e15ca85b42bcbcd5
chenglei <chenglei1@lvmama.com>
2020-08-07 19:57:48 +0800

    加入验证

--- src/main/java/com/microfian/prac/config/WebSecurityConfig.java
+++ src/main/java/com/microfian/prac/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.microfian.prac.config;
 
 import com.microfian.prac.handler.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -22,17 +24,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence charSequence) {
-                return charSequence.toString();
-            }
-
-            @Override
-            public boolean matches(CharSequence charSequence, String s) {
-                return s.equals(charSequence.toString());
-            }
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder() {
+//            @Override
+//            public String encode(CharSequence charSequence) {
+//                BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//                return encoder.encode(charSequence);
+//            }
+//
+//            @Override
+//            public boolean matches(CharSequence charSequence, String s) {
+//                BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//                return encoder.matches(charSequence,s);
+//            }
         });
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bcryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        String s=new BCryptPasswordEncoder().encode("123");
+        System.out.println(s);
     }
 
     @Override
